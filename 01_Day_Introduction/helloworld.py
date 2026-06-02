cc2d370dd2515b1cdcc2ebd2c8088825688df976
jiminN2 <alie1023@naver.com>
2026-06-02 18:21:03 +0900

    "Day 1 Commit"

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -23,3 +23,32 @@ print(type('Asabeneh'))          # String
 print(type([1, 2, 3]))           # List
 print(type({'name': 'Asabeneh'}))  # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Tuple
+
+
+print("I am enjoying 30 Days Of Python Challenge")
+print(2+3+4)
+print(2*3)
+print(3+2)
+print(3-2)
+print(3*2)
+print(3/2)
+print(3**2)
+print(3%2)
+print(3//2)
+
+print(type(10))
+print(type(3.14))
+print(type(1+3j))
+print(type('Abby'))
+print(type([1,3,4,5]))
+print(type({'name':'Abby'})) #dictionary
+print(type({9.8, 3.24, 4.5})) #set
+
+# set {} vs tuple ()
+# set is unordered(순서없음,중복없음) collection of unique items, while tuple is ordered collection of items which can be duplicated.
+# set is mutable(수정가능), while tuple is immutable.
+
+## Tuple
+#변경되면 안 되는 데이터 저장
+# 좌표, 날짜, RGB 값 등
+location = (10.0, 20.0)

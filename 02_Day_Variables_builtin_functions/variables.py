d40f8b5e2cd938f164f23afbe2c3694e11fc4d9d
jiminN2 <alie1023@naver.com>
2026-06-04 22:12:58 +0900

    Day 2 commit

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -1,22 +1,25 @@
 
 # Variables in Python
 
-first_name = 'Asabeneh'
-last_name = 'Yetayeh'
+first_name = 'Elicia'
+last_name = 'Park'
 country = 'Finland'
 city = 'Helsinki'
-age = 250
-is_married = True
+age = 25
+is_married = False
 skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
 person_info = {
-    'firstname': 'Asabeneh',
-    'lastname': 'Yetayeh',
+    'firstname': 'Elicia',
+    'lastname': 'Park',
     'country': 'Finland',
     'city': 'Helsinki'
 }
+favourite_food = ('Pasta', 'Salad', 'Pizza')  # Tuple
+unfavourite_food = {'Hamburger', 'Hamburger', 'Fries'}  # Set
 
 # Printing the values stored in the variables
-
+print(favourite_food)
+print(unfavourite_food)
 print('First name:', first_name)
 print('First name length:', len(first_name))
 print('Last name: ', last_name)
@@ -27,6 +30,7 @@ print('Age: ', age)
 print('Married: ', is_married)
 print('Skills: ', skills)
 print('Person information: ', person_info)
+print('first name: ', first_name, 'last name: ', last_name, 'country: ', country)
 
 # Declaring multiple variables in one line
 
@@ -38,3 +42,5 @@ print('Last name: ', last_name)
 print('Country: ', country)
 print('Age: ', age)
 print('Married: ', is_married)
+
+print(first_name, last_name, country, age, is_married)
